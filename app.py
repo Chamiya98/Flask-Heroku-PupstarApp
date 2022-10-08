e32ff8fbd311c160db1122aae1a17c3e167f363e
Jayashan <cham.jayashan@prodoscore.com>
2022-10-08 21:46:16 +0530

    changes

--- app.py
+++ app.py
@@ -246,8 +246,8 @@ def dogList():
 @app.route('/insertbehaviorPastData', methods=['GET', 'POST'], endpoint='insterbehaviorpastdata')
 def pastDataBehavior():
 
-    breedName = request.form('dogname')
-    behavior = request.form('behavior')
+    breedName = request.form['dogname']
+    behavior = request.form['behavior']
     #password = request.args.get('password')
     #fullname = request.args.get('fullname')
 
