f70307f51a954a97ad0eaa745ca838eb367d1275
Jayashan <cham.jayashan@prodoscore.com>
2022-11-12 18:17:29 +0530

    newchnages

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pyodbc
 import tensorflow as tf
 # Flask utils
 from flask import Flask, redirect, render_template, request, flash, jsonify
-from gunicorn.config import User
+#from gunicorn.config import User
 from keras.models import load_model
 from nltk import PorterStemmer
 from sklearn.decomposition import NMF
@@ -73,8 +73,8 @@ def random_number_with_date():
 
 
 def db_connector():
-    # cnxn = pyodbc.connect(
-    #   'DRIVER={SQL Server};SERVER=34.143.213.182;DATABASE=dogcare;UID=sqlserver;PWD=dogcare123;Trusted_Connection=no')
+    #cnxn = pyodbc.connect(
+    #'DRIVER={SQL Server};SERVER=34.143.213.182;DATABASE=dogcare;UID=sqlserver;PWD=dogcare123;Trusted_Connection=no')
     # return cnxn
 
     cnxn = pyodbc.connect(
@@ -432,11 +432,66 @@ def upload():
             with open(img_url, "wb") as fh:
                 fh.write(base64.b64decode(image))
 
-        breed_pred_label, breed_pred_prob = get_prediction_probability_label(model_breed, img_url,
-                                                                             breeds_class_labels)
+            import cv2
+            #from google.colab.patches import cv2_imshow
+            import matplotlib.pyplot as plt
+            #from google.colab import files
+            #% matplotlib
+            #inline
+
+            #from google.colab import drive
+            #drive.mount('/content/gdrive')
+
+
+            #!wget --no-check-certificate \
+             # extract pre-trained face detector
+            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            #face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface.xml')
+            # cat_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface.xml')
+            print(face_cascade)
+
+            # load color (BGR) image2
+            img = cv2.imread(img_url)
+            # convert BGR image to grayscale
+            # !wget --no-check-certificate \
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #!wget - -no - check - certificate \
+                    #https: // raw.githubusercontent.com / computationalcore / introduction - to - opencv / master / assets / haarcascade_frontalface_default.xml
+            # find faces in image
+            faces = face_cascade.detectMultiScale(gray)
+            # cats = cat_cascade.detectMultiScale(gray, scaleFactor=1.3,
+            #	minNeighbors=10, minSize=(75, 75))
+
+            # print number of faces detected in the image
+            print('Number of faces detected:', len(faces))
+
+            # get bounding box for each detected face
+            for (x, y, w, h) in faces:
+                # add bounding box to color image
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            # convert BGR image to RGB for plotting
+            cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # display the image, along with bounding box
+            plt.imshow(cv_rgb)
+            plt.show()
+
+            # Make predictions
+            if len(faces) == 0:
+                #breed_pred_label, breed_pred_prob = get_prediction_probability_label(breeds_model, breeds_image_path,
+                                                                                     #breeds_class_labels)
+                breed_pred_label, breed_pred_prob = get_prediction_probability_label(model_breed, img_url,
+                                                                                     breeds_class_labels)
+            elif len(faces) > 0:
+                breed_pred_prob = 0;
+                breed_pred_label = 'Cannot mmake predictions for human images. Number of faces detected:', len(faces), 'faces detected.'
+
+
         # print(breed_pred_label + "Breed")
         # return (breed_pred_label, breed_pred_prob)
-        result = breed_pred_label + "& Matching Probability is:" + str(breed_pred_prob)
+        #result = breed_pred_label + "& Matching Probability is:" + str(breed_pred_prob)
+        result = breed_pred_label
         # return Response(response=result)
         # return Response(respons)
         return jsonify(
